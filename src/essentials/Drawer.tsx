e5d5eaaf9d9b2749c42ca126d780f8f5d4bdfb1c
Chit Min Thu <chitminthu2005c@gmail.com>
2025-08-22 17:25:51 +0630

    drawer some change

--- src/essentials/Drawer.tsx
+++ src/essentials/Drawer.tsx
@@ -2,7 +2,17 @@ import { Apple, Bell, Home, Phone, User, X } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faViber } from "@fortawesome/free-brands-svg-icons";
+import { faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
+import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { ModeToggle } from "./ModeToggle";
+// import { faHouse } from "@fortawesome/free-solid-svg-icons";
 type DrawerOpen = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -35,39 +45,55 @@ export default function Drawer({ open, setOpen }: DrawerOpen) {
         >
           <X className="w-7 h-7" />
         </div>
-        <div>
-          Hello
-          <div className="relative w-[150px] h-[150px] border border-white/50 bg-green-500  mx-auto rounded-full">
+        <div className="mt-8">
+          <div className="relative w-[120px] h-[120px] border border-white/50 bg-green-500  mx-auto rounded-full">
             <Image
               src="/iphone.jpg"
               alt="Logo"
               fill
-              className="rounded-full object-cover p-2"
+              className="rounded-full object-cover p-[5px]"
             />
           </div>
+          <span className="flex justify-center mt-3 font-bold italic text-2xl">
+            Lord Shenn
+          </span>
         </div>
-        <div className="flex flex-col items-start space-y-5 px-10">
-          <div className="flex items-center space-x-2">
-            <Home />
+        <div className="flex flex-col gap-3 items-start space-y-5 px-10 mt-8">
+          <div className="flex items-center space-x-5">
+            <FontAwesomeIcon icon={faHouse} className="" size="lg" />
             <span>Home</span>
           </div>
-          <div className="flex items-center space-x-2">
-            <User />
+          <div className="flex items-center space-x-5">
+            <FontAwesomeIcon icon={faUser} className="" size="lg" />
             <span>About</span>
           </div>
-          <div className="flex items-center space-x-2">
-            <Bell />
+          <div className="flex items-center space-x-5">
+            <FontAwesomeIcon icon={faFolderOpen} className="" size="lg" />
             <span>Project</span>
           </div>
-          <div className="flex items-center space-x-2">
-            <Apple />
+          <div className="flex items-center space-x-5">
+            <FontAwesomeIcon icon={faTrophy} className="" size="lg" />
             <span>Skill</span>
           </div>
-          <div className="flex items-center space-x-2">
-            <Phone />
+          <div className="flex items-center space-x-5">
+            <FontAwesomeIcon icon={faAddressCard} className="" size="lg" />
             <span>Contact</span>
           </div>
         </div>
+        <div className="px-10 mt-5">
+          <ModeToggle />
+        </div>
+        <div className="flex justify-between  px-12 gap-5 mt-5">
+            <div className="bg-purple-500 w-9 h-9 rounded-full text-center flex justify-center items-center">
+              <FontAwesomeIcon icon={faViber} className="" size="lg" />
+            </div>
+            <div className="bg-blue-800 w-9 h-9 rounded-full text-center flex justify-center items-center">
+              <FontAwesomeIcon icon={faFacebook} className="" size="lg" />
+            </div>
+            <div className="bg-blue-500 w-9 h-9 rounded-full text-center flex justify-center items-center">
+              <FontAwesomeIcon icon={faTelegram} className="" size="lg" />
+            </div>
+          </div>
       </div>
     </div>
   );
